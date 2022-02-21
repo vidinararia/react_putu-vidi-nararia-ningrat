0add0e438c0f03b922148506d04bac589a58065b
vidinararia <vidinararia@gmail.com>
2022-02-21 22:14:05 +0800

    add branch develop

--- 2_Version Control and Branch Management (Git)/praktikum/web-react-saya/src/index.js
+++ 2_Version Control and Branch Management (Git)/praktikum/web-react-saya/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <h1>React-Js</h1>
+    <h2>Develop</h2>
   </React.StrictMode>,
   document.getElementById('root')
 );
