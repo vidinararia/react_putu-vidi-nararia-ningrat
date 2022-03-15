61775b3fff146456745afe1f6d14aaf680db05d7
vidinararia <vidinararia@gmail.com>
2022-03-15 18:54:04 +0800

    add section 10

---
+++ 10_Intorduction React/praktikum/web-react-saya/src/App.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import contact_us from './contact_us';
+import Home from './Home';
+
+function App() {
+  return (
+    <Home />
+  )
+}
+
+export default App;
